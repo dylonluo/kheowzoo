887fef809df2003fe4229f57f66d15d979c563fd
hezekiahsdev <hezekiahsdev>
2024-11-28 13:06:49 +0000

    fix icon grid

--- app/components/IconGrid.tsx
+++ app/components/IconGrid.tsx
@@ -7,31 +7,31 @@ export default function IconGrid(): JSX.Element {
   return (
     <div className="items-center justify-center w-full relative text-white">
     <div className="bg-[#c56927] grid grid-cols-3 md:grid-cols-6 gap-2 mt-10 px-16 py-6">
-      <Link href="https://dextools.io" target="_blank" rel="noopener noreferrer">
+      <Link href="https://www.dextools.io/app/en/solana/pair-explorer/5tCgQ8g9t7YxupJMmQBYTdXq6fJ8KbwFMrvAhfb9thG" target="_blank" rel="noopener noreferrer">
         <Image
           src="/dextool.png"
           alt="Dextools"
           width={50}
           height={50}
-          className="mx-auto"
+          className="mx-auto hover:scale-110"
         />
       </Link>
-      <Link href="https://solscan.io" target="_blank" rel="noopener noreferrer">
+      <Link href="https://jup.ag/swap/SOL-AiQcnL5gPjEXVH1E1FGUdN1WhPz4qXAZfQJxpGrJpump" target="_blank" rel="noopener noreferrer">
         <Image
           src="/solscan.png"
           alt="Solscan"
           width={50}
           height={50}
-          className="mx-auto"
+          className="mx-auto hover:scale-110"
         />
       </Link>
-      <Link href="https://dex-screen.io" target="_blank" rel="noopener noreferrer">
+      <Link href="https://dexscreener.com/solana/5tCgQ8g9t7YxupJMmQBYTdXq6fJ8KbwFMrvAhfb9thG" target="_blank" rel="noopener noreferrer">
         <Image
           src="/dex-screener.png"
           alt="Dex Screen"
           width={50}
           height={50}
-          className="mx-auto"
+          className="mx-auto hover:scale-110"
         />
       </Link>
       <Link href="https://coingecko.com" target="_blank" rel="noopener noreferrer">
@@ -40,14 +40,14 @@ export default function IconGrid(): JSX.Element {
           alt="Coin Gecko"
           width={100}
           height={100}
-          className="mx-auto"
+          className="mx-auto hover:scale-110"
         />
       </Link>
       <Link
         href="https://telegram.org"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-3xl text-white hover:text-gray-300 mx-auto"
+        className="text-4xl text-green-200 mt-2 py-1 hover:scale-110 hover:text-green-300 mx-auto"
       >
         <FaTelegramPlane />
       </Link>
@@ -55,7 +55,7 @@ export default function IconGrid(): JSX.Element {
         href="https://x.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-3xl text-white hover:text-gray-300 mx-auto"
+        className="text-4xl text-green-200 mt-2 py-1 hover:scale-110 hover:text-green-300 mx-auto"
       >
         <FaTwitter />
       </Link>
