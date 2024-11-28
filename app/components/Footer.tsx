8a4bdf0b2279647e09c5687f4994b30490dd51fc
HezekiahsDevAndroidStudio <hezekiahs.dev@devlabs.com>
2024-11-28 07:35:26 +0100

    modify footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-function Footer() {
+import Link from "next/link";
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-secondary-color text-white py-4 px-6 text-center">
+      <p>&copy; {new Date().getFullYear()} KheowZoo. All rights reserved.</p>
+      <p>
+        Follow us on{" "}
+        <Link href="https://twitter.com/KheowZoo" className="underline">
+          Twitter
+        </Link>
+        .
+      </p>
+    </footer>
+  );
 }
-
-export default Footer
