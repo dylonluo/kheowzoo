579f14723f7a09d899aff988d97adecbcd5ce03d
hezekiahsdev <hezekiahsdev>
2024-11-28 12:18:11 +0000

    fix header view

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+//import Header from "./components/Header";
+//import Footer from "./components/Footer";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -57,9 +57,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
+       
         <main className="min-h-screen">{children}</main>
-        <Footer />
+   
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 //import Image from "next/image";
 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="">
+      <Header />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        <Hero />
       </main>
-  
+      <Footer />
     </div>
   );
 }
