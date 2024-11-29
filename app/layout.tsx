e4e4f68197d5bd3e6ccf19029227f58cc225b786
hezekiahsdev <hezekiahsdev>
2024-11-29 13:18:14 +0000

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-<<<<<<< HEAD
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-=======
->>>>>>> dac5f01691fd85ec9b7bcbd32360efb37a8749f2
 
 export const metadata: Metadata = {
   title: "Kheowzoo",
@@ -45,13 +42,9 @@ export default function RootLayout({
         />
       </head>
       <body className="scroll-smooth">
-<<<<<<< HEAD
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
-=======
-        <main className="">{children}</main>
->>>>>>> dac5f01691fd85ec9b7bcbd32360efb37a8749f2
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 //import Image from "next/image";
 
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+//import Footer from "./components/Footer";
+//import Header from "./components/Header";
 import Hero from "./components/Hero";
 import IconGrid from "./components/IconGrid";
 
