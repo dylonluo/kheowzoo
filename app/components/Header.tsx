fc19312d9b59e4fe88abc99de90326e731163ebf
HezekiahsDevAndroidStudio <hezekiahs.dev@devlabs.com>
2024-12-05 06:34:18 +0100

    remove veiw more

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -61,7 +61,7 @@ export default function Header() {
           Gallery
         </Link>
         <Link
-          href="/https://x.com/i/communities/1846046371033633151"
+          href="https://x.com/i/communities/1846046371033633151"
           target="_blank"
           className="block px-4 py-2 text-lg text-green-500 hover:text-green-300 font-medium hover:underline md:px-0 md:py-0 md:hover:underline"
           onClick={() => setIsMobileMenuOpen(false)}
